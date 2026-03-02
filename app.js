8150c5d7be6095dc70c69b9bf24dfb9193b478fa
elisabichiappini <elisabichiappini@gmail.com>
2026-03-02 12:16:51 +0100

    feat: routers

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const pizzaRouter = require("./routers/pizzas");
 dotenv.config();
 const { PORT } = process.env;
-const port = PORT || 3001;
+const port = PORT || 3000;
 
 app.use(express.json());
+app.use("/pizzas", pizzaRouter);
 
 app.listen(port, () => {
     console.log(`SERVER ATTIVO SU http://localhost:${port}`)
---
+++ prisma/migrations/20260302105239_ciao2/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Pizza` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `available` BOOLEAN NOT NULL DEFAULT true,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
