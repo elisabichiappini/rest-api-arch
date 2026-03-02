b20be70014fe248a472a199c75ea5df9b50a2375
elisabichiappini <elisabichiappini@gmail.com>
2026-03-02 14:33:51 +0100

    feat: resolve app crashed

--- controllers/pizzas.js
+++ controllers/pizzas.js
@@ -1,10 +1,20 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../prisma/client");
 
 const store = (req, res) => {
+    const { name, price, available } = req.body;
+    if(!name || !price || !available) {
+        return res.status(400).send("Dati mancanti");
+    }
+    prisma.pizza.create({ 
+        data: {
+            name:'',
+            price: '',
+            available: ''
+        }
+     });
     res.status(200).send("Creazione pizza");
 }
 
 module.exports = {
-    store
+    store,
 }
