8fb29a84987c3a367402919efc06d221cc0a9876
elisabichiappini <elisabichiappini@gmail.com>
2026-03-02 14:52:42 +0100

    feat: correction

--- controllers/pizzas.js
+++ controllers/pizzas.js
@@ -1,15 +1,16 @@
-const prisma = require("../prisma/client");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const store = (req, res) => {
     const { name, price, available } = req.body;
-    if(!name || !price || !available) {
+    if(!name || isNaN(price)) {
         return res.status(400).send("Dati mancanti");
     }
     prisma.pizza.create({ 
         data: {
             name:'',
             price: '',
-            available: ''
+            available: req.body.available ? true : false,
         }
      });
     res.status(200).send("Creazione pizza");
