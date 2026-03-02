a49f5990c99c129c913f6aeffd97effe2dfaaceb
elisabichiappini <elisabichiappini@gmail.com>
2026-03-02 11:33:05 +0100

    feat: correction error in schema provider

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const { PORT } = process.env;
-const port = PORT || 3000;
+const port = PORT || 3001;
 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`SERVER ATTIVO SU http://localhost${port}`)
+    console.log(`SERVER ATTIVO SU http://localhost:${port}`)
 });
